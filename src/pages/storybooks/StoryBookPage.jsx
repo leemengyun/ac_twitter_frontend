d59fb1d4c584a1539ab8dab0ca9b7dbd554503e4
KFR1230 <su0916111448@gmail.com>
2023-06-07 22:57:50 +0800

    feat: add name component

--- src/pages/storybooks/StoryBookPage.jsx
+++ src/pages/storybooks/StoryBookPage.jsx
@@ -4,13 +4,72 @@ import PageLayout from '../../components/layout/PageLayout';
 
 //import css
 import '../../scss/components/storyBookPage.scss';
-
+import Defaultavatar from '../../assets/images/icon/avatar.svg'
+import Url from '../../assets/images/icon/chat.svg'
 const LayoutNav = () => {
+
+
+  
+  const UserAvatar = ({imgURL,Default}) =>{
+    return (
+      <>
+        <div className='avatar'>
+          <img
+            src={imgURL || Default}
+            alt='userAvatar'
+            className='user_avatar'
+          />
+        </div>
+      </>
+    )
+  }
+
+  const TweetUserName3 = ({name, account , time})=>{
+    return (
+      <>
+        <div className='tweet_username_3'>
+          <span className='user_name'>{name}</span>
+          <span className='user_account'>@{account}</span>
+          <span className='user_time'> ·{time}小時</span>
+        </div>
+      </>
+    )
+  }
+
+  const TweetUserName2 = ({name, account})=>{
+    return (
+      <>
+        <div className='tweet_username_2'>
+          <span className='user_name'>{name}</span>
+          <span className='user_account'>@{account}</span>
+        </div>
+      </>
+    )
+  }
+
+
+
   return (
     <>
-      <PageLayout>
+      <PageLayout >
         {/* 二欄式頁面 */}
-
+        <UserAvatar 
+        Default={Defaultavatar}
+        imgURL={' https://media.licdn.com/dms/image/D5603AQGmA1K4ud_M1g/profile-displayphoto-shrink_400_400/0/1675331260718?e=1691625600&v=beta&t=eR4xDwl0gMzApYrYmf92cOOy8575nA9mrUlZZxvb2Ys'}
+        />
+        <TweetUserName3
+        name={'印度歐爸'}
+        account={'currycurry'}
+        time={3}
+        />
+        <UserAvatar 
+        Default={Defaultavatar}
+        imgURL={'https://pyxis.nymag.com/v1/imgs/4cd/212/81283ed5194911027fd285de7b326616f0-04-end-of-the-world.rsocial.w1200.jpg'}
+        />
+        <TweetUserName2
+        name={'James'}
+        account={'TEOFW'}
+        />
         <div>
           <div className='storybook-container'>
             <h2>Storybooks</h2>
