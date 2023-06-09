336d2ffc59d2d38d83b0ac59f5c3a5e238d94d22
leemengyun <shamifrandy@gmail.com>
2023-06-09 23:06:06 +0100

    feat: ProfileCard 串接 json-server資料

---
+++ src/api/userinfo.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3004';
+
+//要記得export外面才能用
+export const getUserInfo = async () => {
+  try {
+    // 獲得todo end point
+    const res = await axios.get(`${baseUrl}/userinfo`);
+
+    //server 回傳的物件會包在data,所以一定要用.data才會拿到對的資料
+    // console.log(res.data[0].data.user);
+    return res.data[0].data.user;
+  } catch (error) {
+    console.error('[getUser Info failed]', error);
+  }
+};
+
+//要記得export外面才能用
+// export const updateUserInfo = async (payload) => {
+//   try {
+//     // 要帶的資料設定為payload
+//     const { account, name } = payload;
+//     // 獲得todo end point
+//     const res = await axios.patch(`${baseUrl}/userinfo`, {
+//       account,
+//       name,
+//     });
+//     //server 回傳的物件會包在data,所以一定要用.data才會拿到對的資料
+//     return res.data;
+//   } catch (error) {
+//     console.error('[getUser Info failed]', error);
+//   }
+// };
